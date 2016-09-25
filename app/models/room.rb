23e68f0b154667172a9a836e4f5a8b9186f6f1be
Shriyansh-Y <shriyanshyadav94@gmail.com>
2016-09-25 12:52:12 -0400

    Model changes room.rb for search fn

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,8 @@
 class Room < ActiveRecord::Base
   has_many :bookings
   validates :room_number, :capacity, :building, presence: true
+
+  def self.search(search)
+    where("room_number LIKE ? OR capacity LIKE ? OR building LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+  end
 end
